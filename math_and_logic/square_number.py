a8708ce66331e363edf0a7081567696fa79049fd
C.Alexandru <110183848+Ciuflea-Alexandru@users.noreply.github.com>
2026-05-15 10:53:37 +0000

    appended the square_number.py and added second_smallest_number.py

--- math_and_logic/square_number.py
+++ math_and_logic/square_number.py
@@ -8,5 +8,17 @@ def triangle(num):
 def square(num):
     return triangle(num) + triangle(num - 1)
 
-number = 6
+number = 3
 print(square(number))
+
+# Return multiple numbers and their squares in a list of tuples
+
+def square_items_with_tuples(lst: list[int]) -> list[tuple[int, int]]:
+    result = []
+    for n in lst:
+        result.append((n, n * n))
+    
+    return result
+
+list = [1, 2, 3]
+print(square_items_with_tuples(list))
